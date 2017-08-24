0d0426ad7c23728d6e28bfe1cd7045ebce351632
Mark Kircher <mkircher@lappy13.cs.nmsu.edu>
2017-08-24 13:20:25 -0600

    this is an update to the hello world program made earlier

--- lab1/helloworld.java
+++ lab1/helloworld.java
@@ -7,6 +7,7 @@ public class helloworld{
    public static void main(String Args[]){
    
       System.out.println("hello world");
+      System.out.println("goodbye world");
       
       }
       }
